a06c6a657104cb1d77fb3fe85575946d65a0919f
Your Name <you@example.com>
2021-01-17 14:50:11 +0800

    stats

---
+++ GitTesting-master/app/src/main/java/com/example/money/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.money;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.getbase.floatingactionbutton.*;
+
+public class MainActivity extends AppCompatActivity {// implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //set action for the button on the main page
+        FloatingActionButton fab1 = findViewById(R.id.toExpense);
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddTransaction.class);
+                startActivity(intent);
+            }
+        });
+
+   /*     FloatingActionButton fab2 = findViewById(R.id.toIncome);
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddIncome.class);
+                startActivity(intent);
+            }
+        }); */
+    }
+
+}
